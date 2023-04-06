c922d8ed009b5a3fee6bc5d8f69bfd4347d561d7
hadynz <hadyos@gmail.com>
2023-04-07 02:21:21 +1200

    Ready for first demo

--- src/App.tsx
+++ src/App.tsx
@@ -16,8 +16,8 @@ import ReactFlow, {
 import 'reactflow/dist/style.css';
 
 const initialNodes: Node[] = [
-  { id: '1', data: { label: 'Node 1' }, position: { x: 5, y: 5 } },
-  { id: '2', data: { label: 'Node 2' }, position: { x: 5, y: 100 } },
+  { id: '1', data: { label: 'Linear cycles' }, position: { x: 5, y: 5 } },
+  { id: '2', data: { label: 'World Changing Business' }, position: { x: 5, y: 100 } },
 ];
 
 const initialEdges: Edge[] = [{ id: 'e1-2', source: '1', target: '2' }];
